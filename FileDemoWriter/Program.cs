e9f1ff77b174cf5fa93b2a76a870eee18b10e961
Juantrevi <juantrevi70@gmail.com>
2024-01-15 11:47:10 +0000

    Initial commit

---
+++ FileDemoWriter/Program.cs
@@ -0,0 +1,25 @@
+﻿
+const string fileName = @"C:\Users\juant\OneDrive\Escritorio\Dorset\7 - OOP\FileWriterDemo\FileDemoWriter\sample-writer.txt";
+
+try
+{
+    
+    var writer = new StreamWriter(fileName, true);
+    writer.WriteLine("Hello World");
+    writer.WriteLine("From the stream writer");
+
+    for (int i = 0; i < 10; i++)
+    {
+        writer.WriteLine($"Line {i}");
+    }
+    
+    writer.Close();
+}
+catch(Exception e)
+{
+    Console.WriteLine("Failed: " + e.Message);
+}
+finally
+{
+    Console.WriteLine("Finally block");
+}
